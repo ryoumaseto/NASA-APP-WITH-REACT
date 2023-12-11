dea0fadbd622f1183226cf6f0fcd91df02bfadb9
ryoumaseto <ryohma919@gmail.com>
2023-12-11 21:31:47 +0900

    responsive design

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,23 +7,80 @@ const header = () => {
       <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-          Let's look into space
+            Let's look into space
           </span>
           <div
-            className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1 mx-auto"
+            className="justify-between items-center w-full lg:flex lg:w-auto lg:order-1 mx-auto"
             id="mobile-menu-2"
           >
-            <ul className="flex flex-col  mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-
-                <Link to="/" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
-                    Home
+            <input
+              type="checkbox"
+              className="hidden"
+              id="menu-toggle"
+            />
+            <label
+              htmlFor="menu-toggle"
+              className="block lg:hidden cursor-pointer"
+            >
+              <svg
+                className="w-6 h-6 fill-current text-gray-700 dark:text-gray-400"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  className="heroicon-ui"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </label>
+            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+              <li className="lg:hidden">
+                <Link
+                  to="/"
+                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Home
                 </Link>
-                <Link to="/apod" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
-                    APOD
+              </li>
+              <li className="lg:hidden">
+                <Link
+                  to="/apod"
+                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  APOD
                 </Link>
-                <Link to="/mars" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
-                    MARS ROVER
+              </li>
+              <li className="lg:hidden">
+                <Link
+                  to="/mars"
+                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  MARS ROVER
                 </Link>
+              </li>
+              <li className="hidden lg:block">
+                <Link
+                  to="/"
+                  className="py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Home
+                </Link>
+              </li>
+              <li className="hidden lg:block">
+                <Link
+                  to="/apod"
+                  className="py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  APOD
+                </Link>
+              </li>
+              <li className="hidden lg:block">
+                <Link
+                  to="/mars"
+                  className="py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  MARS ROVER
+                </Link>
+              </li>
             </ul>
           </div>
         </div>
--- src/components/mars_rover/MarsRoverForm.js
+++ src/components/mars_rover/MarsRoverForm.js
@@ -25,12 +25,12 @@ const MarsRoverForm = ({ sol, camera, setPages, setCamera, fetchData }) => {
     ];
 
     return (
-        <div className='right-20'>
-            <div className='flex items-center'>
-                <label className='ml-20 font-sans' htmlFor="pages">Max Pages:</label>
-                <input min='1' max='25' type="number" placeholder='10' className="ml-5 rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10 text-right" id="pages" value={sol} onChange={(e) => setPages(e.target.value)} />
-                <label className="ml-20 font-sans" htmlFor="camera">Camera:</label>
-                <select id="camera" className="ml-5 rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10" value={camera} onChange={(e) => setCamera(e.target.value)}>
+        <div className='sm:w-full md:w-1/2 lg:w-1/3 xl:w-1/4'>
+            <div className='flex flex-col sm:flex-row items-center'>
+                <label className='ml-2 sm:ml-20 font-sans' htmlFor="pages">Max Pages:</label>
+                <input min='1' max='25' type="number" placeholder='10' className="ml-2 sm:ml-5 rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10 text-right" id="pages" value={sol} onChange={(e) => setPages(e.target.value)} />
+                <label className="ml-2 sm:ml-20 font-sans" htmlFor="camera">Camera:</label>
+                <select id="camera" className="ml-2 sm:ml-5 rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10" value={camera} onChange={(e) => setCamera(e.target.value)}>
                     {cameras.map((camera) => (
                         <option key={camera.name} value={camera.name}>
                             {camera.full_name}
@@ -38,7 +38,7 @@ const MarsRoverForm = ({ sol, camera, setPages, setCamera, fetchData }) => {
                     ))}
                 </select>
 
-                <button onClick={() => fetchData()} className="ml-3 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+                <button onClick={() => fetchData()} className="ml-2 sm:ml-3 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
                     検索
                 </button>
             </div>
