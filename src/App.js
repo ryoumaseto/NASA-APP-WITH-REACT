1d7c3dde397ba1543bdf23753cfec00b8e5ca6db
ryoumaseto <ryohma919@gmail.com>
2023-12-18 19:58:07 +0900

    memo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Main from './components/apod/Main';
 import Mars from './components/mars_rover/Mars';
 import Home from './components/Home/Home';
+import Memo from './components/Memo/Memo';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/apod" element={<Main />} />
         <Route path="/mars" element={<Mars />} />
+        <Route path="/memo" element={<Memo />} />
       </Routes>
     </Router>
   );
